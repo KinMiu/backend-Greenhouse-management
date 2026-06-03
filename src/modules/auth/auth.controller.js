6737c6996e4646dae6ba3e2cd66dcd8dc3c92c21
KinMiu <prasetyoaldi875@gmail.com>
2026-06-03 15:09:00 +0700

    fix user authorization

--- src/modules/auth/auth.controller.js
+++ src/modules/auth/auth.controller.js
@@ -36,7 +36,7 @@ export const loginController = async (req, res) => {
 
     res.cookie("user_role", result.user.role, {
       httpOnly: false,
-      secure: false,
+      secure: process.env.NODE_ENV === "production",
       maxAge: duration,
       path: "/",
       sameSite: "lax",
