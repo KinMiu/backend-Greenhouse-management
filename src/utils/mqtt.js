b8d69906b00390bff8a5f0804fa2a958477aed2d
KinMiu <prasetyoaldi875@gmail.com>
2026-07-13 14:41:47 +0700

    prisma config

--- src/utils/mqtt.js
+++ src/utils/mqtt.js
@@ -2,8 +2,16 @@ import dotenv from "dotenv";
 import mqtt from "mqtt";
 import logger from "./logger.js";
 
+dotenv.config();
+
 const brokerUrl = process.env.MQTT_BROKER_URL;
-const clientIdPrefix = process.env.MQTT_CLIENT_PREFIX || "GH_iot_system_";
+const clientIdPrefix = process.env.MQTT_CLIENT_PREFIX;
+
+if (!brokerUrl) {
+  logger.error(
+    "CRITICAL: MQTT_BROKER_URL is undefined! Check your environment variables.",
+  );
+}
 
 const mqttClient = mqtt.connect(brokerUrl, {
   username: process.env.MQTT_USERNAME,
@@ -12,11 +20,9 @@ const mqttClient = mqtt.connect(brokerUrl, {
 });
 
 mqttClient.on("connect", () => {
-  logger.info("Backend successfully connected to MQTT Broker");
+  logger.info(`Backend successfully connected to MQTT Broker: ${brokerUrl}`);
 });
 
 mqttClient.on("error", (err) => {
   logger.error("MQTT Connection Error: ", err);
 });
-
-export default mqttClient;
